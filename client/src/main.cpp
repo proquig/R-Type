5180951da23ed2bca37ef67f49932c9bf1e9c1e9
Lucien Perouze <lucien.perouze@epitech.eu>
2016-11-28 15:45:55 +0100

    doc + test window

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,4 +1,20 @@
+#include <SFML/Graphics.hpp>
+
 int main(int ac, char **av)
 {
-  return (0);
+	sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.clear(sf::Color::Black);
+		window.display();
+	}
+
+	return 0;
 }
