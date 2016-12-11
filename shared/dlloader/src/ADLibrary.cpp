416157cc73ff0b9a5ebf79e65bad050ca975d254
Dr-Shadow <dr-shadow@dr-shadow.eu>
2016-12-11 17:27:45 +0100

    DLLoader suggest path from filename (#19)

--- shared/dlloader/src/ADLibrary.cpp
+++ shared/dlloader/src/ADLibrary.cpp
@@ -1,6 +1,6 @@
 #include "ADLibrary.hh"
 
-#if RT_UNIX
+#ifdef RT_UNIX
   #include "CUDLibrary.hh"
 #elif RT_WIN
   #include "CWDLibrary.hh"
@@ -9,6 +9,12 @@
 ADLibrary::ADLibrary(int id, std::string name, std::string path)
     : _id(id), _name(name), _path(path)
 {
+  if (_path == std::string(""))
+#ifdef RT_UNIX
+    _path = std::string("./lib" + _name + ".so");
+#elif RT_WIN
+    _path = std::string(_name + ".dll");
+#endif
 }
 
 IDLibrary* ADLibrary::createLibrary(int id, std::string name, std::string path)
