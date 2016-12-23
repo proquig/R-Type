f886218dc996dbe1308f81878fbdc5f99696faeb
Mimoone <mimoone.ml@gmail.com>
2016-12-23 14:46:37 +0100

    Trying to resolve why git isn't currently working...

---
+++ build/client/Text.hh
@@ -0,0 +1,44 @@
+#ifndef TEXT_HH_
+#define TEXT_HH_
+
+#include <SFML\Graphics\Font.hpp>
+#include <SFML\Graphics\Text.hpp>
+#include "element.hh"
+
+class Text : public AElement
+{
+public:
+
+	void	loadSprites(GLib);
+	void	print(void *);
+	void	move(int, int, float, int);
+	void	destroy();
+
+	//////////////////////////////////////////////
+	// SETTERS
+	//////////////////////////////////////////////
+
+	void			setId(unsigned int);
+	void			setAnimation(std::string);
+	void			setSize(Coords *);
+	void			setFont(std::string);
+	void			setFontSize(int);
+	void			setMaxLength(int);
+	void			setString(std::string);
+
+	//////////////////////////////////////////////
+	// VALUES
+	//////////////////////////////////////////////
+
+private:
+	std::string		_str;
+	std::string		_fontPath;
+	sf::Font		_font;
+	int				_fontSize;
+	sf::Text		_txt;
+	int				_maxLen;
+	bool			_isReady;
+};
+
+
+#endif // !TEXT_HH_
